7602dedc001a3acf1cad180494f65736878a2610
simiaolin <simiao.lin@student.kuleuven.be>
2021-05-11 11:55:30 +0200

    [add] for submitted

--- plotDistribution.py
+++ plotDistribution.py
@@ -1,58 +1,42 @@
-import numpy as np
 import matplotlib.pyplot as plt
 import sys as sys
-import pandas as pd
 
-print(1)
-distibution_file = "/Users/ary/dev/code/trait/bdap_3/src/resources/output/first/2010_03.trip_distribution"
-ranges = []
-cnts = []
-with open(distibution_file) as file:
-    for line in file:
-        range_and_cnt = line.rstrip("\n")
-        range = int(range_and_cnt.split(",")[0])
-        cnt = int(range_and_cnt.split(",")[1])
-        if (cnt < 200 and range < 30000):
-            ranges.append(range)
-            cnts.append(cnt)
+#plot distribution of trip length
+def plotTripLengthDistribution(trip_distribution_file):
+    trip_length = []
+    count_of_length = []
+    with open(trip_distribution_file) as file:
+        for line in file:
+            length_and_count = line.rstrip("\n")
+            trip_length.append(int(length_and_count.split(",")[0]))
+            count_of_length.append(int(length_and_count.split(",")[1]))
+    file.close()
+    plt.bar(trip_length, count_of_length)
+    plt.show()
+
+#plot distribution of airport revenue
+def plotAirportTripRevenueDistribution(airport_trip_revenue_file):
+    times = []
+    revenues = []
+    with open(airport_trip_revenue_file) as file:
+        for line in file:
+            time_and_revenue = line.rstrip("\n")
+            time = time_and_revenue.split()[0]
+            revenue = float(time_and_revenue.split()[1])
+            times.append(time)
+            revenues.append(revenue)
+
+    plt.bar(times, revenues)
+    plt.xticks(rotation=-30, size=8)  # 设置x轴标签旋转角度
+    file.close()
+    plt.show()
 
 
-file.close()
-# plt.bar(ranges, cnts)
+type = sys.argv[1]
+if (type == 'trip_length'):
+    plotTripLengthDistribution(sys.argv[2])
+elif (type == 'trip_revenue'):
+    plotAirportTripRevenueDistribution(sys.argv[2])
+else:
+    print("the plotting type should be [trip_length]  or [trip_revenue]")
 
-revenue_file = "/Users/ary/PycharmProjects/plotTaxiDistanceDistribution/revenue"
-times = []
-revenues = []
-with open(revenue_file) as file:
-    for line in file:
-        time_and_revenue = line.rstrip("\n")
-        time = time_and_revenue.split()[0]
-        revenue = float(time_and_revenue.split()[1])
-        times.append(time[2:])
-        revenues.append(revenue)
-plt.bar(times, revenues)
-file.close()
-# commutes = pd.Series(res)
-# commutes.plot.hist(rwidth=0.9,
-#                    color='#607c8e')
-# plt.title('taxi trip dist distribution')
-# plt.xlabel('trip length')
-# plt.ylabel('counts')
-# plt.grid(axis='y', alpha=0.75)
-# ax2 = fig.add_subplot(122)
-# todo add density and boxplot
-plt.show()
-
-#for bdap
-# plt.yticks(np.arange(0.9, 1, 0.005))
-# plt.xlabel("ensemble size")
-# plt.ylabel("accuracy")
-# ax2.set_ylim([0, 60])
-# a = np.arange(1, 11)
-# b = [0.919985,  0.918705, 0.945388, 0.946881, 0.953924, 0.954606, 0.95814,  0.958652, 0.960231, 0.960112]
-# c = [59, 56.5, 56, 56, 55.6, 55.8, 55.6, 56.4, 55.2, 56]
-# plt.plot(a, b, color="purple")
-# plt.grid(axis='y', linestyle='-')
-# # ax2.plot(a, c, color="pink")
-#
-# plt.show()
